0590caeebdfef31d2bf71bda2dc6412f286ad200
Sven Luethi <dev@svenluthi.com>
2025-11-25 00:13:06 +0100

    modified:   src/netbox_plugin_bind_provisioner/__init__.py Update version string

--- src/netbox_plugin_bind_provisioner/__init__.py
+++ src/netbox_plugin_bind_provisioner/__init__.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 from django.conf import settings
 
-__version__ = "0.9.6"
+__version__ = "0.9.8"
 
 logger = logging.getLogger(__name__)
 

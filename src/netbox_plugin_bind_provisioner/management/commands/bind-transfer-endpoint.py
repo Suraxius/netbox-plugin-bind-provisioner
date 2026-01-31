ca51a6054d57900587b7c05a666e4d15b38a7a5c
Sven Luethi <dev@svenluthi.com>
2026-01-31 09:01:04 +0100

    This reverts commit 8fba131230fe222edfe4cc5de6cb732062d13413.

--- src/netbox_plugin_bind_provisioner/management/commands/bind-transfer-endpoint.py
+++ src/netbox_plugin_bind_provisioner/management/commands/bind-transfer-endpoint.py
@@ -1,5 +1,4 @@
 import logging
-import socket
 import socketserver
 import os
 import threading
@@ -610,52 +609,20 @@ class TCPRequestHandler(DNSBaseRequestHandler):
             traceback.print_exc()
 
 
-class DNSAddressMixin:
-    def _resolve_address(self, server_address, socktype, proto):
-        host, port = server_address
-
-        infos = socket.getaddrinfo(
-            host,
-            port,
-            socket.AF_UNSPEC,
-            socktype,
-            proto,
-            socket.AI_PASSIVE
-        )
-
-        family, _, _, _, sockaddr = infos[0]
-        self.address_family = family
-        return sockaddr
-
-
-class TCPDNSServer(DNSAddressMixin, socketserver.TCPServer):
+class TCPDNSServer(socketserver.TCPServer):
     allow_reuse_address = True
 
     def __init__(self, server_address, handler_class, keyring, tsig_view_map):
-        sockaddr = self._resolve_address(
-            server_address,
-            socket.SOCK_STREAM,
-            socket.IPPROTO_TCP
-        )
-
-        super().__init__(sockaddr, handler_class)
-
+        super().__init__(server_address, handler_class)
         self.keyring = keyring
         self.tsig_view_map = tsig_view_map
 
 
-class UDPDNSServer(DNSAddressMixin, socketserver.UDPServer):
+class UDPDNSServer(socketserver.UDPServer):
     allow_reuse_address = True
 
     def __init__(self, server_address, handler_class, keyring, tsig_view_map):
-        sockaddr = self._resolve_address(
-            server_address,
-            socket.SOCK_DGRAM,
-            socket.IPPROTO_UDP
-        )
-
-        super().__init__(sockaddr, handler_class)
-
+        super().__init__(server_address, handler_class)
         self.keyring = keyring
         self.tsig_view_map = tsig_view_map
 
